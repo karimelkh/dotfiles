34a79c515cbceadd084a57c58b713c147393cfb5
karimelkh <karimelkhanoufi22@gmail.com>
2024-02-18 22:32:19 +0100

    update

--- dwm/dwm/config.h
+++ dwm/dwm/config.h
@@ -3,12 +3,12 @@
 // THIS IS A CHANGE
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 20;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int splitstatus        = 0;        /* 1 for split status items */
-static const char *splitdelim        = ";";       /* Character used for separating status */
+static const char *splitdelim        = " | ";       /* Character used for separating status */
 static const char *fonts[]          = { "Hack Nerd Font:size=12" };
 static const char dmenufont[]       = "size=12";
 static const char col_gray3[]       = "#f1be9b";
