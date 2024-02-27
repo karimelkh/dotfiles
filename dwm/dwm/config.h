0bf76ca75ae483adaa3ce423d49a758c3150b015
karimelkh <karimelkhanoufi22@gmail.com>
2024-02-27 12:58:55 +0100

    add vanitygaps

--- dwm/dwm/config.h
+++ dwm/dwm/config.h
@@ -3,13 +3,13 @@
 // THIS IS A CHANGE
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 0;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int splitstatus        = 0;        /* 1 for split status items */
@@ -138,6 +138,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} }, // increase all gaps
+	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} }, // decrease all gaps
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} }, // toggle gaps on and off
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} }, // reset gaps back to default
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
