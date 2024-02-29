331fe47614348b35f54136133e74d8c78e51a28f
karimelkh <karimelkhanoufi22@gmail.com>
2024-02-29 17:04:42 +0100

    update tags

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -37,9 +37,11 @@ static const unsigned int alphas[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "󰑴", "󰇧", };
-//static const char *tags[] = { "", "", "", "󰻀", "", "󰑴", "󰇧", "󰈹", "󰀼", "󰣇", "", "󰲓", "󰌪", "" };
-//static const char *tags[]   = { "󰈹", "󰣇", "󰻀", "", "", "󰀼", "󰌪", "" };
+static const char *tags[] = { "", "󰣇", "󰈹", "󰂺", "", "", "󰚩", "󰌪", "" };
+// static const char *tags[] = { "", "", "", "", "󰑴", "󰇧", };
+// static const char *tags[] = { "", "", "󰚩", "", "󰂺", "", "", "", "", "", "", "", };
+// static const char *tags[] = { "", "", "", "󰻀", "", "󰑴", "󰇧", "󰈹", "󰀼", "󰣇", "", "󰲓", "󰌪", "" };
+// static const char *tags[] = { "󰈹", "󰣇", "󰻀", "", "", "󰀼", "󰌪", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
