e653438bb5966e6a479400256fd31734c95585ef
karimelkh <karimelkhanoufi22@gmail.com>
2024-03-29 12:51:03 +0000

    add flexipatch directory
    contains flexipatch code - not configured yet

---
+++ suckless/flexipatch/dmenu-flexipatch/util.h
@@ -0,0 +1,13 @@
+/* See LICENSE file for copyright and license details. */
+
+#ifndef MAX
+#define MAX(A, B)               ((A) > (B) ? (A) : (B))
+#endif
+#ifndef MIN
+#define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#endif
+#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+#define LENGTH(X)               (sizeof (X) / sizeof (X)[0])
+
+void die(const char *fmt, ...);
+void *ecalloc(size_t nmemb, size_t size);
